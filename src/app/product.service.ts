c0ddb9173f4c01b5219250c05e3ee85ffa0c6321
CHidave291 <dukelere@gmail.com>
2019-10-29 12:41:10 +0100

    Service to get album data modified

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,14 +3,15 @@ import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
+
 export class ProductService {
-  _albulmUrl = '../assets/album.json'
+  private _albumUrl = '../assets/album.json'
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albulmUrl)
-      .map(Response => Response.json)
+    return this._http.get(this._albumUrl)
+      .map(response => response.json)
   }
 
 }
